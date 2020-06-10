ea90e66ee3c59162b695b64192c521c276983198
mavrickkc <mavrickkc@yahoo.com>
2020-06-10 18:01:14 +0000

    Lab 2

--- main.js
+++ main.js
@@ -79,7 +79,7 @@ function get(serviceNowTable, callback) {
  *   Will be HTML text if hibernating instance.
  * @param {error} callback.error - The error property of callback.
  */
-function POST(serviceNowTable, callback) {
+function post(serviceNowTable, callback) {
 
   // Initialize return arguments for callback
   let callbackData = null;
